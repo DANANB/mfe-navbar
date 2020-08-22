b72377286f18b07f58474881215b01131dd58cf1
Daniel Anbarestani <Daniel.Anbarestani@iCloud.com>
2020-08-22 19:16:30 +0200

    Initial commit

---
+++ src/root.component.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Root(props) {
+  return (
+    <Router>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/">{props.name}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink className="nav-link" activeClassName="active" to="/todos">
+              View todos
+            </NavLink>
+            <NavLink className="nav-link" activeClassName="active" to="/add">
+              Add todo
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Router>
+  );
+}
